299f8b9ef837a776430f358dc8471a83d667fb1e
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-11-19 17:29:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,6 +15,27 @@ var gameOver, restart;
 var bgg,bgg2;
 localStorage["HighestScore"] = 0;
 
+function preload(){
+  trex_running =   loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+  
+  bgg=loadImage("bgg.jpg");
+}
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   
